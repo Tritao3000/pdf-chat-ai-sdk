afd25794161ac344ac9ee149e2620e9cb6f4c1db
Tiago Pereira <tiagorp2004@gmail.com>
2023-12-08 00:13:02 +0000

    updates

--- src/lib/pdf-loader.ts
+++ src/lib/pdf-loader.ts
@@ -1,23 +1,32 @@
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { env } from "./config";
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { env } from './config';
+import { UnstructuredLoader } from 'langchain/document_loaders/fs/unstructured';
 
 export async function getChunkedDocsFromPDF() {
   try {
-    const loader = new PDFLoader(env.PDF_PATH);
-    const docs = await loader.load();
+    let loaders = [
+      new PDFLoader('ro.pdf'),
+      new PDFLoader('joao.pdf'),
+      new PDFLoader('intro.pdf'),
+      new UnstructuredLoader('https://augustalabs.co'),
+    ];
+    let pages = [];
+    for (let loader of loaders) {
+      pages.push(...(await loader.load()));
+    }
 
     // From the docs https://www.pinecone.io/learn/chunking-strategies/
     const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
+      chunkSize: 500,
+      chunkOverlap: 100,
     });
 
-    const chunkedDocs = await textSplitter.splitDocuments(docs);
+    const chunkedDocs = await textSplitter.splitDocuments(pages);
 
     return chunkedDocs;
   } catch (e) {
     console.error(e);
-    throw new Error("PDF docs chunking failed !");
+    throw new Error('PDF docs chunking failed !');
   }
 }
