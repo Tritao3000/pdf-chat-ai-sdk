948b99fe2fc7e8b3b879ab016a3495e1e6bdd54a
Tiago Pereira <tiagorp2004@gmail.com>
2023-12-08 16:32:23 +0000

    minor changes

--- src/lib/prompt-templates.ts
+++ src/lib/prompt-templates.ts
@@ -7,10 +7,9 @@ Follow Up Input: {question}
 Standalone question:`;
 
 // Actual question you ask the chat and send the response to client
-export const QA_TEMPLATE = `You are an enthusiastic AI assistant designed to awnser questions about Augusta Labs. Use the following pieces of information given to you to answer the question at the end.
+export const QA_TEMPLATE = `You are an enthusiastic AI assistant designed to awnser questions about Augusta Labs. Use the txt file given to you to awnser the question and help the user.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-Use the custom pdfs that you have access to awnser to the questions
+If the question is not related to the information on the txt file, politely respond that you are tuned to only answer questions that are related to the context.
 
 Question: {question}
 Helpful answer:`;
