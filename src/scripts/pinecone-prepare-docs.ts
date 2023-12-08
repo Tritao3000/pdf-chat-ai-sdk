9424d0e83772df90b5ccede889bfdb7713d9a40f
Tiago Pereira <tiagorp2004@gmail.com>
2023-12-08 02:32:01 +0000

    it works. Fine tunning and data updates to be done

--- src/scripts/pinecone-prepare-docs.ts
+++ src/scripts/pinecone-prepare-docs.ts
@@ -1,6 +1,6 @@
-import { getChunkedDocsFromPDF } from "@/lib/pdf-loader";
-import { embedAndStoreDocs } from "@/lib/vector-store";
-import { getPineconeClient } from "@/lib/pinecone-client";
+import { getChunkedDocsFromPDF } from '@/lib/pdf-loader';
+import { embedAndStoreDocs } from '@/lib/vector-store';
+import { getPineconeClient } from '@/lib/pinecone-client';
 
 // This operation might fail because indexes likely need
 // more time to init, so give some 5 mins after index
@@ -8,12 +8,12 @@ import { getPineconeClient } from "@/lib/pinecone-client";
 (async () => {
   try {
     const pineconeClient = await getPineconeClient();
-    console.log("Preparing chunks from PDF file");
+    console.log('Preparing chunks from PDF file');
     const docs = await getChunkedDocsFromPDF();
     console.log(`Loading ${docs.length} chunks into pinecone...`);
-    await embedAndStoreDocs(pineconeClient, docs);
-    console.log("Data embedded and stored in pine-cone index");
+    await embedAndStoreDocs(pineconeClient);
+    console.log('Data embedded and stored in pine-cone index');
   } catch (error) {
-    console.error("Init client script failed ", error);
+    console.error('Init client script failed ', error);
   }
 })();
