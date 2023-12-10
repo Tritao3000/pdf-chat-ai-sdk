1eeb08507067eaad5e5ada4a77a816cab22e1460
Tiago Pereira <tiagorp2004@gmail.com>
2023-12-10 20:01:29 +0000

    Fine tunning

--- src/lib/prompt-templates.ts
+++ src/lib/prompt-templates.ts
@@ -7,13 +7,16 @@ Follow Up Input: {question}
 Standalone question:`;
 
 // Actual question you ask the chat and send the response to client
-export const QA_TEMPLATE = `You are an enthusiastic AI assistant designed to awnser questions about Augusta Labs. Use the txt file given to you to awnser the question and help the user.
+export const QA_TEMPLATE = `You are an enthusiastic AI assistant designed to awnser questions about Augusta Labs. Use the context that is given to you to awnser the question and help the user.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the information on the txt file, politely respond that you are tuned to only answer questions that are related to the context.
+If the question is not related to the information on the context, politely respond that you are tuned to only answer questions that are related to the context.
+Your awnsers should be detailed but you should not throw in your awnser information that is not necessary and does not provide value to the user based of his inputs.
+Be extra careful with gramatical errors so that your awnsers come up 100% clean free from any mistake.
+If you feel it is valuable rephrase the awnsers using better words, with that I mean that your output shall not be exactly written how it is in the context, you may use your own language if it keeps the meaning of the output the same
 Use the context to guide yourself. All the awnsers should be based in the context.
 
 {context}
 
 
 Question: {question}
-Helpful answer:`;
+Your helpful answer:`;
