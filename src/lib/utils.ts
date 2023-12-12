409511ea8510c14f9b9eba4711dea87c793ab92c
Tiago Pereira <tiagorp2004@gmail.com>
2023-12-12 17:30:20 +0000

    character escaping

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -38,7 +38,8 @@ export function formattedText(inputText: string) {
   return inputText
     .replace(/\n+/g, ' ') // Replace multiple consecutive new lines with a single space
     .replace(/(\w) - (\w)/g, '$1$2') // Join hyphenated words together
-    .replace(/\s+/g, ' '); // Replace multiple consecutive spaces with a single space
+    .replace(/\s+/g, ' ') // Replace multiple consecutive spaces with a single space
+    .replace(/"/g, '');
 }
 
 // Default UI Message
