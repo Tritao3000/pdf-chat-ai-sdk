8bc287607001ad5e5539d12f1ffeb2a3a86dd6af
Tiago Pereira <tiagorp2004@gmail.com>
2023-12-12 21:16:24 +0000

    small change

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -43,7 +43,7 @@ export function formattedText(inputText: string) {
 }
 
 export function formattedResponse(inputText: string) {
-  return inputText.replace(/"/g, '');
+  return inputText.replace(/"/g, '').replace(/clients'/g, 'clients');
 }
 
 // Default UI Message
