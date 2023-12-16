bda20d6de9d25aed1286636572a71370a1867c39
Tiago Pereira <tiagorp2004@gmail.com>
2023-12-16 14:29:39 +0000

    small change in formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -38,8 +38,7 @@ export function formattedText(inputText: string) {
   return inputText
     .replace(/\n+/g, ' ') // Replace multiple consecutive new lines with a single space
     .replace(/(\w) - (\w)/g, '$1$2') // Join hyphenated words together
-    .replace(/\s+/g, ' ') // Replace multiple consecutive spaces with a single space
-    .replace(/"/g, '');
+    .replace(/\s+/g, ' '); // Replace multiple consecutive spaces with a single space
 }
 
 export function formattedResponse(inputText: string) {
